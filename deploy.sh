aef32e7a78a0175779597a39c1542da7bd2cfec1
ambuila106 <brandon.ambuila@unillanos.edu.co>
2023-12-15 11:27:59 -0500

    coorrect path

--- deploy.sh
+++ deploy.sh
@@ -9,4 +9,4 @@ git init
 git add -A
 
 git commit -m 'new development'
-git push -f https://github.com/ambuila106/festival-universitario.git master:gh-pages
+git push -f https://github.com/ambuila106/festi-sugest.git master:gh-pages
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    publicPath: '/festival-universitario/',
+    publicPath: '/festi-sugest/',
 }
